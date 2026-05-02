62fc2b490279464d728afd6ad041f423861da069
Ajeet Kumar Pandit <kumarajeet19022004@gmail.com>
2026-05-02 18:17:08 +0530

    feat: configure remote image patterns and add Netlify build settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'qlkrdipyrqikvafhmeio.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
